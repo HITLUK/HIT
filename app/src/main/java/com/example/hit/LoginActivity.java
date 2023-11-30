163c7f50a2354e673a1f8e09822a28065197673b
HITLUK <113688274+HITLUK@users.noreply.github.com>
2023-12-01 01:37:20 +0700

    sa

--- app/src/main/java/com/example/hit/LoginActivity.java
+++ app/src/main/java/com/example/hit/LoginActivity.java
@@ -1,50 +1,37 @@
 package com.example.hit;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.os.Bundle;
-
-import android.content.Intent;
-import android.util.Patterns;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.EditText;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.hit.databinding.ActivityLoginBinding;
 import com.google.android.material.textfield.TextInputLayout;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class LoginActivity extends AppCompatActivity {
-DBUsers db;
-Context context;
     EditText email, password;
+    private ActivityLoginBinding binding;
     Button login;
-    ListView listView;
-    TextView register;
     boolean isEmailValid, isPasswordValid;
-    Cursor cursor;
     TextInputLayout emailError, passError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        db = new DBUsers(this);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
-        register = (TextView) findViewById(R.id.register);
         emailError = (TextInputLayout) findViewById(R.id.emailError);
         passError = (TextInputLayout) findViewById(R.id.passError);
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,16 +39,7 @@ Context context;
             }
         });
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // redirect to RegisterActivity
-               Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-               startActivity(intent);
-            }
-
-        });
-    }
+        }
 
     public void SetValidation() {
         if (email.getText().toString().isEmpty()) {
@@ -71,8 +49,6 @@ Context context;
             isEmailValid = true;
             emailError.setErrorEnabled(false);
         }
-
-        // Check for a valid password.
         if (password.getText().toString().isEmpty()) {
             passError.setError(getResources().getString(R.string.password_error));
             isPasswordValid = false;
@@ -84,12 +60,36 @@ Context context;
             passError.setErrorEnabled(false);
         }
 
-        if (isEmailValid && isPasswordValid) {
-            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
-            db.insert("admin","admin","Kirill","Kuznetsov","admin");
-
+        if (isEmailValid && isPasswordValid&&email.getText().toString().equals("admin")&&password.getText().toString().equals("adminka123")) {
+            Toast.makeText(getApplicationContext(), "Вход успешно выполнен", Toast.LENGTH_SHORT).show();
+            scanCode();
         }
 
     }
-
+    private void scanCode()
+    {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+    }
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+            builder.setTitle("Код сотрудника:");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
 }
--- app/src/main/java/com/example/hit/Reposit.java
+++
@@ -1,5 +0,0 @@
-package com.example.hit;
-
-public class Reposit {
-
-}
